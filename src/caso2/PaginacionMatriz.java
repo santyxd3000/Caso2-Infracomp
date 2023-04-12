c9305b866f8351603058343ba78b34adb672d843
santyxd3000 <s.pardom@uniandes.edu.co>
2023-04-11 23:55:06 -0500

    buen formato

--- src/caso2/PaginacionMatriz.java
+++ src/caso2/PaginacionMatriz.java
@@ -16,97 +16,116 @@ public class PaginacionMatriz {
 		this.mp = mp;
 	}
 	
-	
-	public void calcularMatriz(int fila, int columna, String nombreMatriz, int pagina, int desplazamiento) {
-		if (desplazamiento >= tp) {
-			pagina++;
-			desplazamiento = 0;
-		}
-		
-		System.out.println("[" + nombreMatriz + "-" + fila + "-" + columna + "]," + pagina + "," + desplazamiento);
-		desplazamiento += te;
-	}
-	
-	
 
 	
-	public static void calcularMatrizA1(int numRows, int numCols, int pageSize, int intSize) {
-	    int pageIndexA = 0; // índice de página para matriz A
-	    int pageOffsetA = 0; // offset de página para matriz A
-
-	    for (int i = 0; i < numRows; i++) {
-	        for (int j = 0; j < numCols; j++) {
+	public int[][][] calcularReferenciasA() {
+		int[][][] matriz = new int[nf][nc][2];
+		int pagina = 0;
+		int desplazamiento = 0;
+		for (int i = 0; i < nf; i++) {
+	        for (int j = 0; j < nc; j++) {
 	            // Verificar si la página está llena para matriz A
-	            if (pageOffsetA >= pageSize) {
-	                pageIndexA++;
-	                pageOffsetA = 0;
+	            if (desplazamiento >= tp) {
+	                pagina++;
+	                desplazamiento = 0;
 	            }
 
-	            // Insertar elemento en la matriz 
-	            System.out.println("[A-" + i + "-" + j + "]," + pageIndexA + "," + pageOffsetA);
-	            pageOffsetA += intSize;
+	           matriz[i][j][0] = pagina;
+	           matriz[i][j][1] = desplazamiento;
+	           
+	           desplazamiento += te;
+	           
+	          
 	        }
 	    }
+		
+		return matriz;
 	}
 	
-	
-	public static void calcularMatrizB(int numRowsA, int numColsA, int pageSize, int intSize) {
-		int lastIndexA = numRowsA * numColsA - 1; // último índice de la matriz A
-	    int pageIndexB = lastIndexA / (pageSize / intSize); // último índice de la matriz B
-	    int pageOffsetB = ((lastIndexA % (pageSize / intSize)) * intSize + intSize) % pageSize; // offset de página para matriz B
-
-	    
-	    
-	   //Existe un caso que ocurre cuando se cambia de página justo al añadir una nueva matriz
-		   if (pageOffsetB == 0) {
-			   pageIndexB++;
+	public int[][][] calcularReferenciasB() {
+		int elementosMatrizA = nf*nc-1;
+		int pagina = elementosMatrizA / (tp/te);
+		int desplazamiento = ((elementosMatrizA % (tp / te)) * te + te) % tp;
+		
+		int[][][] matriz = new int[nf][nc][2];
+		
+		   if (desplazamiento == 0) {
+			   pagina++;
 		   }
 	    // offset de página para matriz B
 
-	    for (int i = 0; i < numRowsA; i++) {
-	        for (int j = 0; j < numColsA; j++) {
+	    for (int i = 0; i < nf; i++) {
+	        for (int j = 0; j < nc; j++) {
+	        	
+	            if (desplazamiento >= tp) {
+	                pagina++;
+	                desplazamiento = 0;
+	            }
 	            // Insertar elemento en la matriz B
 	            
-	            System.out.println("[B-" + i + "-" + j + "]," + pageIndexB + "," + pageOffsetB);
-
-	            pageOffsetB += intSize;
-	            if (pageOffsetB >= pageSize) {
-	                pageIndexB++;
-	                pageOffsetB = 0;
-	            }
+	            matriz[i][j][0] = pagina;
+	            matriz[i][j][1] = desplazamiento;
+	            
+	            desplazamiento += te;
+	        	
+	        	
 	        }
 	    }
+		
+		
+		return matriz;
 	}
-
 	
-	public static void calcularMatrizC(int numRowsA, int numColsA, int pageSize, int intSize) {
-		int lastIndexA = numRowsA * numColsA - 1;
-		int lastIndexB = numRowsA * numColsA - 1;
-		int pageOffsetB = ((lastIndexA % (pageSize / intSize)) * intSize + intSize) % pageSize;// último índice de la matriz A
-	    int pageIndexC = (lastIndexA + lastIndexB) / (pageSize / intSize); // último índice de la matriz B
-	    int pageOffsetC = ((lastIndexB % (pageSize / intSize)) * intSize + pageOffsetB + intSize) % pageSize; // offset de página para matriz B
 
-	    
-	    
-	   //Existe un caso que ocurre cuando se cambia de página justo al añadir una nueva matriz
-		   if (pageOffsetC == 0) {
-			   pageIndexC++;
+
+	public int[][][] calcularReferenciasC() {
+		int elementosMatrizA = nf*nc-1;
+		int elementosMatrizB = elementosMatrizA;
+		int desplazamientoB = ((elementosMatrizA % (tp / te)) * te + te) % tp;
+		int pagina = (elementosMatrizA + elementosMatrizB)/ (tp/te);
+		int desplazamiento = ((elementosMatrizB % (tp / te)) * te + desplazamientoB + te) % tp;
+		
+		int[][][] matriz = new int[nf][nc][2];
+		
+		   if (desplazamiento == 0) {
+			   pagina++;
 		   }
 	    // offset de página para matriz B
 
-	    for (int i = 0; i < numRowsA; i++) {
-	        for (int j = 0; j < numColsA; j++) {
+	    for (int i = 0; i < nf; i++) {
+	        for (int j = 0; j < nc; j++) {
+	        	
+	            if (desplazamiento >= tp) {
+	                pagina++;
+	                desplazamiento = 0;
+	            }
 	            // Insertar elemento en la matriz B
 	            
-	            System.out.println("[C-" + i + "-" + j + "]," + pageIndexC + "," + pageOffsetC);
-
-	            pageOffsetC += intSize;
-	            if (pageOffsetC >= pageSize) {
-	                pageIndexC++;
-	                pageOffsetC = 0;
-	            }
+	            matriz[i][j][0] = pagina;
+	            matriz[i][j][1] = desplazamiento;
+	            
+	            desplazamiento += te;
+	        	
+	        	
 	        }
 	    }
+	    return matriz;
+		
+	}
+	
+	
+	public void imprimirReferencias() {
+		int[][][] matrizA = calcularReferenciasA();
+		int[][][] matrizB = calcularReferenciasB();
+		int[][][] matrizC = calcularReferenciasC();
+		
+		for (int i = 0; i<nf;i++) {
+			for (int j = 0; j <nc;j++) {
+				System.out.println("[A-" + i + "-" + j + "]," + matrizA[i][j][0] + "," + matrizA[i][j][1]);
+				System.out.println("[B-" + i + "-" + j + "]," + matrizB[i][j][0] + "," + matrizB[i][j][1]);
+				System.out.println("[C-" + i + "-" + j + "]," + matrizC[i][j][0] + "," + matrizC[i][j][1]);
+			}
+		}
 	}
 
 	public int getNf() {
@@ -153,19 +172,13 @@ public class PaginacionMatriz {
 		
 		
 		
-		int numRows = 10;
-		int numCols = 10;
-		int pageSize = 256;
+		int numRows = 4;
+		int numCols = 3;
+		int pageSize = 16;
 		int intSize = 4;
-		PaginacionMatriz paginacion = new PaginacionMatriz(numRows,numCols,pageSize,intSize,5);
+		PaginacionMatriz paginacion = new PaginacionMatriz(numRows,numCols,intSize,pageSize,5);
 		
-		paginacion.calcularMatrizA1(numRows,numCols,pageSize,intSize);
-		paginacion.calcularMatrizB(numRows, numCols, pageSize, intSize);
-		paginacion.calcularMatrizC(numRows, numCols, pageSize, intSize);
-		
-		
-		
-		
+		paginacion.imprimirReferencias();
 		
 	}
 	}
